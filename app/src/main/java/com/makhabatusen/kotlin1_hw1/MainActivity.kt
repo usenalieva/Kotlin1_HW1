fb419a17d54316679a22d99e5d10c65abe74a09b
usenalieva <mahusya12@mail.ru>
2021-04-13 01:24:44 +0600

    Initial commit

---
+++ app/src/main/java/com/makhabatusen/kotlin1_hw1/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.makhabatusen.kotlin1_hw1
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.makhabatusen.kotlin1_hw1.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var ui: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        ui = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(ui.root)
+        init()
+    }
+
+    private fun init() {
+        ui.textViewMain.text = intent?.getStringExtra(KEY_TEXT_MAIN)
+        ui.btnConfirmMain.setOnClickListener {
+            if (ui.editTextMain.text.isNullOrBlank()) {
+                Toast.makeText(this, getString(R.string.cant_be_empty), Toast.LENGTH_SHORT).show()
+            } else {
+                val intent = SecondActivity.newIntent(this, ui.editTextMain.text.toString())
+                startActivity(intent)
+            }
+        }
+    }
+
+    companion object {
+        private const val KEY_TEXT_MAIN: String = "key_main"
+        fun newIntent(
+            context: Context?, message: String
+        ): Intent? {
+            val intent = Intent(context, MainActivity::class.java)
+            intent.putExtra(KEY_TEXT_MAIN, message)
+            return intent
+        }
+    }
+}
---
+++ app/src/main/java/com/makhabatusen/kotlin1_hw1/SecondActivity.kt
@@ -0,0 +1,41 @@
+package com.makhabatusen.kotlin1_hw1
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.makhabatusen.kotlin1_hw1.databinding.ActivitySecondBinding
+
+class SecondActivity : AppCompatActivity() {
+    private lateinit var ui: ActivitySecondBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        ui = ActivitySecondBinding.inflate(layoutInflater)
+        setContentView(ui.root)
+        init()
+    }
+
+    private fun init() {
+        ui.textViewSecond.text = intent?.getStringExtra(KEY_TEXT)
+        ui.btnConfirmSecond.setOnClickListener {
+            if (ui.editTextSecond.text.isNullOrBlank()) {
+                Toast.makeText(this, getString(R.string.cant_be_empty), Toast.LENGTH_SHORT).show()
+            } else {
+                val intent = MainActivity.newIntent(this, ui.editTextSecond.text.toString())
+                startActivity(intent)
+            }
+        }
+    }
+
+    companion object {
+        private const val KEY_TEXT = "key_second"
+        fun newIntent(
+            context: Context?, message: String
+        ): Intent? {
+            val intent = Intent(context, SecondActivity::class.java)
+            intent.putExtra(KEY_TEXT, message)
+            return intent
+        }
+    }
+}
